d7fe21ddebc879eb38e5b85031ec982c7da5ea92
HuanLin98 <61424630+HuanLin98@users.noreply.github.com>
2020-05-14 00:07:58 +0800

    Update ListingCrawler.java
    real version of UNSUCCESSFUL attempt in incorporating crawling first 3 pages of each genre

--- src/main/java/ai/preferred/crawler/steamGames/master/ListingCrawler.java
+++ src/main/java/ai/preferred/crawler/steamGames/master/ListingCrawler.java
@@ -1,68 +1,90 @@
-package ai.preferred.crawler.tutorial.master;
+package ai.preferred.crawler.steamGames.master;
 
-import ai.preferred.crawler.tutorial.entity.Paper;
-import ai.preferred.venom.Handler;
+import ai.preferred.crawler.EntityCSVStorage;
+import ai.preferred.venom.Crawler;
 import ai.preferred.venom.Session;
-import ai.preferred.venom.Worker;
-import ai.preferred.venom.job.Scheduler;
+import ai.preferred.venom.SleepScheduler;
+import ai.preferred.venom.fetcher.AsyncFetcher;
+import ai.preferred.venom.fetcher.Fetcher;
 import ai.preferred.venom.request.Request;
-import ai.preferred.venom.response.VResponse;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
+import ai.preferred.venom.request.VRequest;
+import ai.preferred.venom.validator.EmptyContentValidator;
+import ai.preferred.venom.validator.StatusOkValidator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ai.preferred.crawler.steamGames.entity.Game;
+import ai.preferred.venom.job.Scheduler;
+import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.Jsoup;
+import org.jsoup.select.Elements;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URIBuilder;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.io.IOException;
 
-import java.util.List;
+public class ListingCrawler {
+    // Create session keys for CSV printer to print from handler
+    static final Session.Key<EntityCSVStorage<Game>> STORAGE_KEY = new Session.Key<>();
 
-public class TutorialHandler implements Handler {
+    // You can use this to log to console
+    private static final Logger LOGGER = LoggerFactory.getLogger(ListingCrawler.class);
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(TutorialHandler.class);
+    public static void main(String[] args) {
+        // Get file to save to
+        final String filename = "data/games.csv";
 
-  /**
-   * Exercise 6: Parsing the response from the crawl.
-   * <p>
-   * The handle method will be called once the page has been successfully
-   * crawled by our fetcher. You should use this space to extract the
-   * relevant information you require. For this exercise you are required
-   * to extract all paper name and url and use it to create {@code Paper}
-   * object. Lastly, add all the {@code Paper} objects into papers array
-   * list for the test to pass.
-   * </p>
-   *
-   * @param request   request fetched.
-   * @param response  venom response received.
-   * @param scheduler scheduler used for this request.
-   * @param session   session variables defined when the crawler is initiated.
-   * @param worker    provides the ability to run code in a separate thread.
-   */
-  @Override
-  public void handle(Request request, VResponse response, Scheduler scheduler, Session session, Worker worker) {
-    // Log when there's activity
-    LOGGER.info("Processing {}", request.getUrl());
+        // Start CSV printer
+        try (EntityCSVStorage<Game> storage = new EntityCSVStorage<>(filename)) {
 
-    // The array list to put your results
-    final List<Paper> papers = session.get(TutorialCrawler.PAPER_LIST_KEY);
+            // Let's init the session, this allows us to retrieve the array list in the
+            // handler
+            final Session session = Session.builder().put(STORAGE_KEY, storage).build();
 
-    // Some vars you may need
-    final String html = response.getHtml();
-    final Document document = response.getJsoup();
-    
-    // Get all the papers
-    int counter = 1;
-    Elements el = document.select("#page > div > div > div > div.content > div > article > div > ul > li");
-    System.out.println("*********:" + el.size());
-    for (Element e : el) {
-      
-        final Element aEl = e.selectFirst("a");
-        Paper paper = new Paper(aEl.text(), aEl.attr("abs:href"));
-        papers.add(paper);
-        // System.out.println();
-        // System.out.print(counter++ + aEl.text() + ":");
-        // System.out.print(aEl.attr("href"));
-        
+            // Start crawler
+            try (Crawler crawler = createCrawler(createFetcher(), session).start()) {
+                LOGGER.info("starting crawler...");
+
+                Document doc = Jsoup.connect("https://store.steampowered.com/").get();
+                
+                // filter out game genres from the steam website
+                Elements tabs = doc.select("div.popup_menu_twocol > div.popup_menu > a.popup_menu_item");
+                System.out.println(tabs);
+                
+                for (Element tab : tabs) {
+                    String url = tab.attr("href");
+                    System.out.println(url);
+                    if (url.contains("tags")) {
+                        String baseUrl = url.replace("?snr=1_4_4__12", "");
+                        for (int i = 1; i < 3; i++ ) {
+                            HashMap<String, String> headers = new HashMap<>();
+                            headers.put("#p", "" + i);
+                            crawler.getScheduler().add(new VRequest(url, headers), new ListingHandler());
+                        }
+                        crawler.getScheduler().add(new VRequest(url), new ListingHandler());
+                    }
+                }
+
+            } catch (Exception e) {
+                LOGGER.error("Could not run crawler: ", e);
+            }
+
+        } catch (IOException e) {
+            LOGGER.error("unable to open file: {}, {}", filename, e);
+        }
     }
 
-  }
+    private static Fetcher createFetcher() {
+        return AsyncFetcher.builder()
+                .setValidator(new EmptyContentValidator(), new StatusOkValidator(), new ListingValidator()).build();
+    }
+
+    private static Crawler createCrawler(Fetcher fetcher, Session session) {
+
+        return Crawler.builder().setFetcher(fetcher).setSession(session)
+                .setSleepScheduler(new SleepScheduler(1500, 3000)).build();
+    }
 
 }
