52e8be14579bcc46631672c15a1318229aa71655
HuanLin98 <61424630+HuanLin98@users.noreply.github.com>
2020-05-15 02:23:26 +0800

    Update ListingValidator.java

--- src/main/java/ai/preferred/crawler/steamGames/master/ListingValidator.java
+++ src/main/java/ai/preferred/crawler/steamGames/master/ListingValidator.java
@@ -4,28 +4,19 @@ import ai.preferred.venom.request.Request;
 import ai.preferred.venom.response.Response;
 import ai.preferred.venom.response.VResponse;
 import ai.preferred.venom.validator.Validator;
-import org.jsoup.nodes.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.jsoup.select.Elements;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.utils.URIBuilder;
-import java.io.IOException;
-import java.net.URISyntaxException;
-
 
 public class ListingValidator implements Validator {
     private static final Logger LOGGER = LoggerFactory.getLogger(ListingValidator.class);
 
     @Override
     public Validator.Status isValid(Request request, Response response) {
-        
+
         final VResponse vResponse = new VResponse(response);
+        String jsonString = vResponse.getHtml();
 
-        final Document document = vResponse.getJsoup();
-
-        System.out.println("validating " + document.title());
-        if (document.title().startsWith("Browsing")) {
+        if (jsonString.substring(0,10).contains("success")){
             System.out.println("valid");
             return Status.VALID;
         }
